0e5744c8409ba360fedbb15943a0a70190abe30e
Philipp Zins <philipp.zins@mercateo.com>
2017-02-04 21:30:12 +0100

    finished tokenizer

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 #[derive(Debug,PartialEq)]
 pub enum Token {
     Number(String),
-    // String(String),
-    // Name(String),
+    String(String),
+    Name(String),
     SomethingElse,
     ParenOpening,
     ParenClosing,
@@ -12,7 +12,7 @@ pub fn tokenizer(input: &str) -> Vec<Token> {
     let mut tokens: Vec<Token> = vec![];
 
     let mut char_iter = input.chars().peekable();
-    while let Some(mut c) = char_iter.next() {
+    while let Some(c) = char_iter.next() {
         match c {
             c if c.is_whitespace() => (),
             '(' => tokens.push(Token::ParenOpening),
@@ -25,14 +25,46 @@ pub fn tokenizer(input: &str) -> Vec<Token> {
                     Some(&'0'...'9') => true,
                     _ => false,
                 } {
-                    value.push(c);
-                    c = match char_iter.next() {
-                        Some(c) => c,
-                        None => break,
+                    if let Some(c) = char_iter.next() {
+                        value.push(c);
                     }
                 }
                 tokens.push(Token::Number(value));
             }
+            'a'...'z' => {
+                let mut value = String::new();
+                value.push(c);
+
+                while match char_iter.peek() {
+                    Some(&'a'...'z') => true,
+                    _ => false,
+                } {
+                    if let Some(c) = char_iter.next() {
+                        value.push(c);
+                    }
+                }
+                tokens.push(Token::Name(value));
+            }
+            '"' => {
+                let mut value = String::new();
+                // skip opening "
+
+                while match char_iter.peek() {
+                    Some(&'"') | None => false,
+                    _ => true,
+                } {
+                    if let Some(c) = char_iter.next() {
+                        value.push(c);
+                    }
+                }
+                tokens.push(Token::String(value));
+
+                // skip closing ""
+                if char_iter.peek() == Some(&'"') {
+                    char_iter.next();
+                }
+            }
+            // TODO: Use Option here...
             _ => tokens.push(Token::SomethingElse),
         }
     }
--- tests/test.rs
+++ tests/test.rs
@@ -4,24 +4,15 @@ use the_super_tiny_compiler::*;
 
 #[test]
 fn tokenizer_works() {
-    let input = "(add 2 (subtract 4 2))";
-    // let input = "(add 22 (subtract 4 2))";
+    // let input = "(add 2 (subtract 4 2))";
+    let input = "(add 22 \"ff\" (subtract 4 2))";
 
     let tokens = vec![Token::ParenOpening,
-                      Token::SomethingElse,
-                      Token::SomethingElse,
-                      Token::SomethingElse,
-                      Token::Number("2".to_string()),
-                      //   Token::Number("22".to_string()),
+                      Token::Name("add".to_string()),
+                      Token::Number("22".to_string()),
+                      Token::String("ff".to_string()),
                       Token::ParenOpening,
-                      Token::SomethingElse,
-                      Token::SomethingElse,
-                      Token::SomethingElse,
-                      Token::SomethingElse,
-                      Token::SomethingElse,
-                      Token::SomethingElse,
-                      Token::SomethingElse,
-                      Token::SomethingElse,
+                      Token::Name("subtract".to_string()),
                       Token::Number("4".to_string()),
                       Token::Number("2".to_string()),
                       Token::ParenClosing,
